973fac7818716596c488bf9ddddb375543003ae4
DeSlakator <lece.desl@gmail.com>
2018-02-23 11:04:19 +0300

    c0

---
+++ src/main/java/ru/desl/committest/Main.java
@@ -0,0 +1,12 @@
+package ru.desl.committest;
+/*
+ * Created by DeSlakator on 23.02.2018.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        // it's our first commit
+    }
+
+}
