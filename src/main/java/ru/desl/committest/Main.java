6fc468ed606e0fef1549dcf4f6581f35bdd607c3
DeSlakator <lece.desl@gmail.com>
2018-02-23 11:46:58 +0300

    FTB003: first commit
    Сейчас все сделаем по правилам
    
    FTB003: second commit
    
    Second commit and first push all changes
    
    FTB003: third commit with push
    
    Third commit
    
    FTB003: all done

--- src/main/java/ru/desl/committest/Main.java
+++ src/main/java/ru/desl/committest/Main.java
@@ -27,5 +27,17 @@ public class Main {
     }
 
 
+    public static void FTB003() {
+
+        // first commit
+
+        // second commit and push it
+
+        // third commit with push
+
+        // this was last commit
+
+    }
+
 
 }
